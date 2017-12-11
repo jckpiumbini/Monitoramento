50055b4a932933c9639633a83c6923ac05c4d31e
jckpiumbini <indiorv@gmail.com>
2017-12-11 18:32:00 -0200

    Update monitor_mem.sh

--- monitor_mem.sh
+++ monitor_mem.sh
@@ -8,7 +8,7 @@ TOKEN=""
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
 if [ $SIZE -le $LIMIT ]; then
-        MSG="Servidor InstaGuard-Azure. Limite de memoria excedido. Memoria disponivel $SIZE"
+        MSG="Servidor Nome do Servidor. Limite de memoria excedido. Memoria disponivel $SIZE"
         /usr/bin/curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MSG" 2&> /dev/null
         echo 3 > /proc/sys/vm/drop_cache
 fi
